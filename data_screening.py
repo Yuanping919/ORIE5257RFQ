d8176658f569e383e8eec8e9032fea3e99c0d115
WestBrick <yuanping818@outlook.com>
2022-11-14 12:10:07 -0500

    test data

--- data_screening.py
+++ data_screening.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-data = pd.read_excel("RfqData.xlsx")
+data = pd.read_excel("RfqData.xlsx", sheet_name=1)
 data.Traded.replace('MISSED', 0, inplace=True)
 data.Traded.replace('DONE', 1, inplace=True)
-data.to_csv("training_data.csv", index=False)
+data.to_csv("test_data.csv", index=False)
 
 b0 = data[data.Bond=='Bond_0']
 b1 = data[data.Bond=='Bond_1']
